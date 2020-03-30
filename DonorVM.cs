1738960a61ebc88e64ef41cdbcf684b6104a64c0
StuartRutty <62902567+StuartRutty@users.noreply.github.com>
2020-03-30 13:54:16 -0400

    Add files via upload

---
+++ DonorVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonorTracker
+{
+    public class DonorVM : ViewModelBase
+    {
+        Donor _donor;
+
+        public DonorVM(Donor donor)
+        {
+            _donor = donor;
+        }
+
+        public DonorVM(DonorVM vmDonor)
+        {
+            _donor = new Donor { Email = vmDonor.Email, Name = vmDonor.Name };
+        }
+
+        public DonorVM(string Name, string Email)
+        {
+            _donor = new Donor { Email = Email, Name = Name};
+        }
+
+        public string Name
+        {
+            get { return _donor.Name; }
+            set
+            {
+                _donor.Name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public string Email
+        {
+            get { return _donor.Email; }
+            set
+            {
+                _donor.Email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+
+     }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace DonorTracker
+{
+    public class ViewModel : ViewModelBase
+    {
+        private DonorVM _donor;
+        private ObservableCollection<DonorVM> _donors;
+        private ICommand _addCommand;
+        private ICommand _removeCommand;
+
+        public DonorVM Donor
+        {
+            get
+            {
+                return _donor;
+            }
+            set
+            {
+                _donor = value;
+                NotifyPropertyChanged("Donor");
+            }
+        }
+
+        public ObservableCollection<DonorVM> Donors
+        {
+            get
+            {
+                return _donors;
+            }
+            set
+            {
+                _donors = value;
+                NotifyPropertyChanged("Donors");
+            }
+        }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                if (_addCommand == null)
+                {
+                    _addCommand = new RelayCommand(param => this.Add(), null);
+                }
+                return _addCommand;
+            }
+        }
+
+        public ICommand RemoveCommand
+        {
+            get
+            {
+                if (_removeCommand == null)
+                {
+                    _removeCommand = new RelayCommand(param => this.Remove(), null);
+                }
+                return _removeCommand;
+            }
+        }
+
+        public ViewModel()
+        {
+            Donor = new DonorVM("", "");
+            Donors = new ObservableCollection<DonorVM>();
+        }
+
+        private void Add()
+        {
+            Donor = new DonorVM("", "");
+            Donors.Add(Donor);
+        }
+
+        private void Remove()
+        {
+            Donors.Remove(Donor);
+        }
+
+    }
+}
